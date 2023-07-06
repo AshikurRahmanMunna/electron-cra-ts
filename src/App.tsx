fb1bc919f1e0bf211f878c72cd1a78f724731fe2
Ashikur Rahman Munna <ashikurrahmanmunna3@gmail.com>
2023-07-06 17:19:02 +0600

    import error fixed

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home/Home'
 import Login from './pages/Login/Login'
-import Register from './pages/Register/Register'
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
       </Routes>
     </div>
   )
